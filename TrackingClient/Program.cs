3c5c74eadff358bd22a3de72748e906391c26f1b
kiftmin <brad.minnaar@gmail.com>
2021-12-11 13:26:56 +0200

    Create docker-image.yml

--- TrackingClient/Program.cs
+++ TrackingClient/Program.cs
@@ -24,24 +24,24 @@ namespace TestClass
                 .Build()
                 .Run();
 
-            //// Get environment variables
-            //var config = new ConfigurationBuilder()
-            //    .AddEnvironmentVariables("")
-            //    .Build();
-            //// You need to add these lines for accessing outside of Docker
-            //var url = config["ASPNETCORE_URLS"] ?? "http://*:5000";
-            //var env = config["ASPNETCORE_ENVIRONMENT"] ?? "Development";
+            // Get environment variables
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("")
+                .Build();
+            // You need to add these lines for accessing outside of Docker
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            var env = config["ASPNETCORE_ENVIRONMENT"] ?? "Development";
 
-            //var host = new WebHostBuilder()
-            //    .UseKestrel()
-            //    .UseUrls(url)
-            //    .UseEnvironment(env)
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .UseIISIntegration()
-            //    .UseStartup<Startup>()
-            //    .Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls(url)
+                .UseEnvironment(env)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
-            //host.Run();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
