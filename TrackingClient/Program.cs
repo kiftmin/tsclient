f9121b64b961aaaee0879bb183bf17a33f958f08
kiftmin <brad.minnaar@gmail.com>
2022-01-05 09:58:54 +0200

    Fix bug with Configuration settings

--- TrackingClient/Program.cs
+++ TrackingClient/Program.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using TestConsole;
 using TrackingClient;
@@ -18,25 +19,29 @@ namespace TestClass
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            //////CreateHostBuilder(args)
-            //////    .Build()
-            //////    .Run();
+
+            ////CreateHostBuilder(args)
+            ////    .Build()
+            ////    .Run();
 
             // Get environment variables
             var config = new ConfigurationBuilder()
                 .AddEnvironmentVariables("")
+                .AddJsonFile("appsettings.json",optional:false)
                 .Build();
             // You need to add these lines for accessing outside of Docker
             var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
             var env = config["ASPNETCORE_ENVIRONMENT"] ?? "Development";
 
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseUrls(url)
                 .UseEnvironment(env)
-                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseContentRoot(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)) //Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
@@ -44,16 +49,16 @@ namespace TestClass
             host.Run();
         }
 
-        //////public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //////    Host.CreateDefaultBuilder(args)
-        //////        .ConfigureWebHostDefaults(webBuilder =>
-        //////        {
-        //////            webBuilder
-        //////                //.UseKestrel()
-        //////                //.UseContentRoot(Directory.GetCurrentDirectory())
-        //////                .UseUrls("http://*:5000")
-        //////                //.UseIISIntegration()
-        //////                .UseStartup<Startup>();
-        //////        });
+        ////public static IHostBuilder CreateHostBuilder(string[] args) =>
+        ////    Host.CreateDefaultBuilder(args)
+        ////        .ConfigureWebHostDefaults(webBuilder =>
+        ////        {
+        ////            webBuilder
+        ////                .UseKestrel()
+        ////                .UseContentRoot(Directory.GetCurrentDirectory())
+        ////                .UseUrls("http://*:5000")
+        ////                .UseIISIntegration()
+        ////                .UseStartup<Startup>();
+        ////        });
     }
 }
